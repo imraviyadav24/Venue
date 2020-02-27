516ae19bed12c2a69b4cce7e3b57f853ced06cb0
ravi yadav <raviyadav00063@gmail.com>
2020-02-27 23:39:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './resources/styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/header_footer/Header';
+import Featured from './components/featured';
+import VenueNfo from './components/venueNfo';
+import Highlights from './components/Highlights';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App" style={{height:"1500px",background:'cornflowerblue'}}>
+        <Header />
+        <Featured />
+        <VenueNfo />
+        <Highlights />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Highlights/Discount.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+import Slide from 'react-reveal/Slide';
+
+class Discount extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Discount;
