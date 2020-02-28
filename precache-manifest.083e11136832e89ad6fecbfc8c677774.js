f1eaf88adb48ac06dba9976ffca497b8bd969a51
ravi yadav <raviyadav00063@gmail.com>
2020-02-28 07:53:25 +0530

    Updates

---
+++ precache-manifest.083e11136832e89ad6fecbfc8c677774.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16676b5581b2db5ba1d501259f655e85",
+    "url": "/venue/index.html"
+  },
+  {
+    "revision": "8ed71b3bcd97ebfb27c0",
+    "url": "/venue/static/css/main.d6fa7445.chunk.css"
+  },
+  {
+    "revision": "23db6459320355fca037",
+    "url": "/venue/static/js/2.d95cbfc9.chunk.js"
+  },
+  {
+    "revision": "ee8db149588b919769b40f758096f482",
+    "url": "/venue/static/js/2.d95cbfc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed71b3bcd97ebfb27c0",
+    "url": "/venue/static/js/main.66175aa8.chunk.js"
+  },
+  {
+    "revision": "535bcbc233a2c1ddfb1a",
+    "url": "/venue/static/js/runtime-main.d20fee26.js"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/venue/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/venue/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/venue/static/media/slide_two.3a3ffd36.jpg"
+  }
+]);
